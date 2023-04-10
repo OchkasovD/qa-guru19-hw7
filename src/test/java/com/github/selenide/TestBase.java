6924cc3d7a43680286caa6cc2c639de48f6a6fba
Dmitry Ochkasov <loki.vsk@gmail.ru>
2023-04-10 20:22:19 +0300

    init

---
+++ src/test/java/com/github/selenide/TestBase.java
@@ -0,0 +1,11 @@
+package com.github.selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+    public class TestBase {
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.browserSize = "1200x800";
+        }
+}
---
+++ src/test/java/com/github/selenide/TestGit.java
@@ -0,0 +1,21 @@
+package com.github.selenide;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestGit extends TestBase {
+    @org.junit.jupiter.api.Test
+    void findSoftAssertions() {
+        open("https://github.com/");
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        $$ ("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("ul.filterable-active").shouldHave(text("SoftAssertions"));
+        $("ul.filterable-active").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5"));
+    }
+}
+
